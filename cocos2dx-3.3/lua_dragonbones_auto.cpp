52c9ac0ef34ce7164dbf7d85a77f741803daf407
lite3 <lite3@qq.com>
2015-03-22 01:26:36 +0800

    update lua_dragonbones_auto.cpp

--- cocos2dx-3.3/lua_dragonbones_auto.cpp
+++ cocos2dx-3.3/lua_dragonbones_auto.cpp
@@ -6201,6 +6201,53 @@ int lua_dragonbones_DBCCArmatureNode_getCCSlot(lua_State* tolua_S)
 
     return 0;
 }
+int lua_dragonbones_DBCCArmatureNode_getInnerBoundingBox(lua_State* tolua_S)
+{
+    int argc = 0;
+    dragonBones::DBCCArmatureNode* cobj = nullptr;
+    bool ok  = true;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if COCOS2D_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"db.DBCCArmatureNode",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (dragonBones::DBCCArmatureNode*)tolua_tousertype(tolua_S,1,0);
+
+#if COCOS2D_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_dragonbones_DBCCArmatureNode_getInnerBoundingBox'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 0) 
+    {
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_dragonbones_DBCCArmatureNode_getInnerBoundingBox'", nullptr);
+            return 0;
+        }
+        cocos2d::Rect ret = cobj->getInnerBoundingBox();
+        rect_to_luaval(tolua_S, ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "db.DBCCArmatureNode:getInnerBoundingBox",argc, 0);
+    return 0;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_dragonbones_DBCCArmatureNode_getInnerBoundingBox'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_dragonbones_DBCCArmatureNode_unregisterFrameEventHandler(lua_State* tolua_S)
 {
     int argc = 0;
@@ -6519,6 +6566,7 @@ int lua_register_dragonbones_DBCCArmatureNode(lua_State* tolua_S)
         tolua_function(tolua_S,"getAnimation",lua_dragonbones_DBCCArmatureNode_getAnimation);
         tolua_function(tolua_S,"getArmature",lua_dragonbones_DBCCArmatureNode_getArmature);
         tolua_function(tolua_S,"getCCSlot",lua_dragonbones_DBCCArmatureNode_getCCSlot);
+        tolua_function(tolua_S,"getInnerBoundingBox",lua_dragonbones_DBCCArmatureNode_getInnerBoundingBox);
         tolua_function(tolua_S,"unregisterFrameEventHandler",lua_dragonbones_DBCCArmatureNode_unregisterFrameEventHandler);
         tolua_function(tolua_S,"getCCDisplay",lua_dragonbones_DBCCArmatureNode_getCCDisplay);
         tolua_function(tolua_S,"advanceTime",lua_dragonbones_DBCCArmatureNode_advanceTime);
